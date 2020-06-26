6708bdd1b8f4bc34e2c7e4839dc782a0847a8aab
Maslyaev <maslyaevalexandr@gmail.com>
2020-06-26 12:17:51 +0300

    Перенос

--- app/src/main/java/com/example/universityranking/StartView.kt
+++ app/src/main/java/com/example/universityranking/StartView.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import kotlin.math.sign
 
 class StartView : Fragment() {
 
@@ -21,20 +22,19 @@ class StartView : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_start_view, container, false)
-
-        activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        val decor = activity!!.window.decorView
+/*
+        activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN*/
+        /*val decor = activity!!.window.decorView*/
 
         signIn = view.findViewById(R.id.signIn_start)
         signUp = view.findViewById(R.id.signUp_start)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val decor: View = activity!!.window.decorView
             decor.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
+        }*/
 
         signIn!!.setOnClickListener {
-            var fragment:AuthenticationView? = AuthenticationView()
             activity!!.supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.fragment_container, AuthenticationView())
@@ -42,17 +42,15 @@ class StartView : Fragment() {
                     .commit()
         }
 
+        signUp!!.setOnClickListener {
+            activity!!.supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, RegistrationView())
+                    .addToBackStack(null)
+                    .commit()
+        }
         return view
     }
-/*
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                StartView().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }*/
+
+
 }
